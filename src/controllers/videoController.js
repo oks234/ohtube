07aae5902136312ccaff120178902ec221e704ae
Kyungseok Oh <bestspx@gmail.com>
2024-12-04 15:41:00 +0900

    5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,13 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch Video" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit Video" });
+const fakeUser = {
+  username: "Ken",
+  loggedIn: false,
+};
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch Video" });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit Video" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
