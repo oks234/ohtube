81289ce042292be6464030983918fefb099a4ae0
Kyungseok Oh <bestspx@gmail.com>
2024-12-04 10:28:33 +0900

    3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  return res.send("Home");
 };
-
-const handleHome = (req, res) => {
+const login = (req, res) => {
   return res.send("Home");
 };
 
 app.use(logger);
-app.get("/", handleHome);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚗`);
