ee08f1f0155d3054a6fefb988f7a879c2966e74c
Kyungseok Oh <bestspx@gmail.com>
2024-12-04 10:40:58 +0900

    4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -6,16 +6,29 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const home = (req, res) => {
-  return res.send("Home");
-};
-const login = (req, res) => {
-  return res.send("Home");
-};
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit user");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚗`);
