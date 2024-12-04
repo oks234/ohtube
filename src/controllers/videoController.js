c46d6ee9e97aec219b87100e93144fffd10d4cee
Kyungseok Oh <bestspx@gmail.com>
2024-12-04 15:51:31 +0900

    5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,9 +2,27 @@ export const trending = (req, res) => {
   const videos = [
     {
       title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 miniutes ago",
+      views: 59,
+      id: 1,
     },
     {
       title: "Video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 miniutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 miniutes ago",
+      views: 59,
+      id: 3,
     },
   ];
   return res.render("home", { pageTitle: "Home", videos });
