f4fab20fcc32d2ef7973198237d2c96248115c3a
Kyungseok Oh <bestspx@gmail.com>
2024-12-05 16:01:05 +0900

    7.2 Creating Account part Three

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,7 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -6,6 +9,14 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  console.log("User", this.password);
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(this.password || "", salt);
+  this.password = hash;
+  console.log("Hashed", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
