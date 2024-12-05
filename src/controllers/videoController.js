53921004b9cf3f7ae9cf5035217511a7c8c0714a
Kyungseok Oh <bestspx@gmail.com>
2024-12-05 14:41:03 +0900

    6.22 Edit Video part Three

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,16 +26,18 @@ export const getEdit = async (req, res) => {
 };
 export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id);
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found" });
   }
-  video.title = req.body.title;
-  video.description = req.body.description;
-  video.hashtags = req.body.hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -48,8 +50,8 @@ export const postUpload = async (req, res) => {
       title,
       description,
       hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
     });
     return res.redirect("/");
   } catch (error) {
