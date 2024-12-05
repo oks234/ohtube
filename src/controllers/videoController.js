34aee37230141023cffc97933ae167f8efae635a
Kyungseok Oh <bestspx@gmail.com>
2024-12-05 14:18:58 +0900

    6.19 Video Detail

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,10 +5,12 @@ export const trending = async (req, res) => {
   console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
+  const video = await Video.findById(id);
   return res.render("watch", {
-    pageTitle: `Watching`,
+    pageTitle: `Watching ${video.title}`,
+    video,
   });
 };
 export const getEdit = (req, res) => {
