ce1709e9ac6ecd8ea33b77b7451462bf77db7d26
Kyungseok Oh <bestspx@gmail.com>
2024-12-05 11:32:24 +0900

    6.17 Exceptions and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,15 +24,17 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((tag) => `#${tag}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((tag) => `#${tag}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
