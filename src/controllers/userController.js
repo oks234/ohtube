5cdfc0f2cda7962327da66c4272b45aba1b752fc
Kyungseok Oh <bestspx@gmail.com>
2024-12-05 16:18:22 +0900

    7.3 Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,23 @@
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
-  return res.render("join", { pageTitle: "Create Account" });
+  return res.render("join", { pageTitle: "Join" });
 };
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  const { email, username, password, name, location } = req.body;
+  const { email, username, password, password2, name, location } = req.body;
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ email }, { username }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "The username/email is already exists.",
+    });
+  }
   await User.create({ email, username, password, name, location });
   return res.redirect("/login");
 };
