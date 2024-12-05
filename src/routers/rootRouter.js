b0c2f845d392142ab7fe8bdaa58373745020b944
Kyungseok Oh <bestspx@gmail.com>
2024-12-05 16:34:07 +0900

    Login part One

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, login, postJoin } from "../controllers/userController";
+import {
+  getJoin,
+  getLogin,
+  postJoin,
+  postLogin,
+} from "../controllers/userController";
 import { search, trending } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", trending);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
