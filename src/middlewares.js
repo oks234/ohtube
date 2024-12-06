bac26a757174b39d937b9ee0635a77051f9e86ce
Kyungseok Oh <bestspx@gmail.com>
2024-12-06 11:47:07 +0900

    8.1 Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,17 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedInUser = req.session.user || {};
   next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return res.redirect("/");
+  }
+  return next();
+};
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,12 +6,17 @@ import {
   postLogin,
 } from "../controllers/userController";
 import { search, trending } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", trending);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
