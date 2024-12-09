9d86d02ecde54e98708a4c9246e8c24f4937d662
Kyungseok Oh <bestspx@gmail.com>
2024-12-09 17:54:57 +0900

    13.2-3 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,14 +1,44 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+
+const handleDownload = () => {
+
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    const videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
   video.srcObject = stream;
   video.play();
-  //  console.log(stream);
 };
 
+init();
 startBtn.addEventListener("click", handleStart);
