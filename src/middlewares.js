f0ca16576d2fa1cab17778a4bb273596c26ca6d8
Kyungseok Oh <bestspx@gmail.com>
2024-12-11 09:20:13 +0900

    changed env varibales

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,8 +5,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
   region: "ap-northeast-2",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
   },
 });
 
